1ed711929e38eba60a38194a6b11202d08b35b84
Guilherme Estevao <guilhermewilliann@hotmail.com>
2023-12-03 09:51:08 -0300

    initial commit

---
+++ src/pages/ResultPage.js
@@ -0,0 +1,21 @@
+import { Button } from "../App";
+
+export default function ResultPage({ result, backToGame }) {
+
+    const style = {
+        fontSize: "24px"
+    };
+
+    return (
+        <div className="App">
+            <ul style={style}>
+                <li>Você escolheu: {result.playerDecision}</li>
+                <li>Maquina escolheu: {result.computerDecision}</li>
+                <li>Seus pontos: {result.playerScore}</li>
+                <li>Pontos da máquina: {result.computerScore}</li>
+            </ul>
+
+            <Button name={"Voltar"} handleClick={backToGame}  />
+        </div>
+    );
+}
